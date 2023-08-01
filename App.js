fb7844f18346ed6d0cbe6491b83177f9c811bf38
JoshuaOliver8 <69984179+JoshuaOliver8@users.noreply.github.com>
2023-08-01 19:19:55 -0400

    Exercise 2 completed, ESLint added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I've been hungry all my life</Text>
+      <Text>I have been hungry all my life</Text>
       <StatusBar style="auto" />
     </View>
   );
